d4807d8bd5ba178b39261d72b35ac01c9e0a88c7
Elias Babenko <63197354+eliasbnk@users.noreply.github.com>
2024-11-16 07:02:32 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,9 +8,7 @@ nvm use node
 unset EMAIL
 unset PASSWORD
 echo "Setup complete!"
-echo "Uncomment out: "
-echo -e "\n\n\n# source /opt/homebrew/opt/spaceship/spaceship.zsh\n# source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh\n# source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh\n# source /opt/homebrew/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh\n
-# ZSH_THEME=\"spaceship\"\n# plugins=(zsh-autosuggestions zsh-syntax-highlighting zsh-autocomplete)\n# eval \"$(zoxide init zsh)\"\n# eval \"$(fzf --zsh)\"\n"
-echo "in ~/.zshrc"
+echo "Uncomment out the first 15 lines in ~/.zshrc"
+echo "And then run: source ~/.zshrc"
 rm ~/.config/build.sh ~/.config/setup_vscode.sh ~/.config/setup_ssh_git.sh
 exit 0
