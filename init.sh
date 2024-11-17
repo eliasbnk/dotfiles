9b2ab19cd15e8c89d2b2140282f310df380f8190
Elias Babenko <63197354+eliasbnk@users.noreply.github.com>
2024-11-16 23:31:12 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -39,18 +39,6 @@ install_brew_packages() {
 }
 
 apply_zshrc(){
-if ! grep -q 'eval "$(/opt/homebrew/bin/brew shellenv)"' $HOME/.zprofile; then
-      cat <<EOF >>$HOME/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
-export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-export HOMEBREW_CASK_OPTS="--no-quarantine"
-export NVM_DIR="\$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-EOF
-fi
-source $HOME/.zprofile
-
 if [ -f $HOME/.zshrc ]; then
     cp $HOME/.zshrc $HOME/.zshrc_backup_$(date +'%Y%m%d%H%M%S')
 fi
