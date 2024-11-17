1b4b9025521e4837c5ea01b253a4f810a5e625cc
Elias Babenko <63197354+eliasbnk@users.noreply.github.com>
2024-11-16 20:45:17 -0800

    Update flake.nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,62 +1,158 @@
+/*
+    !CHECKME: 
+    NIX DOCS: https://mynixos.com/options
+    HOMEBREW DOCS: https://docs.brew.sh/Manpage
+    MACOS SETTINGS DOCS: https://macos-defaults.com
+ */
+
 {
-  description = "nix-darwin system flake";
+  description = "Nix-Darwin Flake";
 
   inputs = {
+    # !-----------↓↓ DO NOT EDIT ↓↓----------!
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    # !-----------↑↑ DO NOT EDIT ↑↑----------!
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
   let
-
-    user = builtins.toString (builtins.exec "whoami" []);
-
-    flake_device = builtins.getEnv "flake_device" or "macbook";
-
-    brewCasks = builtins.splitString "\n" (builtins.readFile ./brew_casks.txt);
-
-    brewFormulae = builtins.splitString "\n" (builtins.readFile ./brew_formulae.txt);
-
-    dockApps = builtins.splitString "\n" (builtins.readFile ./dock_apps.txt);
-
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, config, ... }: {
       
-      # READ: https://mynixos.com/nix-darwin/option/environment.shellAliases
-      # environment.shellAliases = {};
-
+      # -------- Homebrew Configuration --------
       homebrew = {
-        enable = true;
-        brews = brewFormulae;
-        casks = brewCasks;
+        # !-----------↓↓ DO NOT EDIT ↓↓----------!
+        enable = true;  # Enable Homebrew package manager
+        # !-----------↑↑ DO NOT EDIT ↑↑----------!
 
-        # install apps from App Store
-        # READ: https://mynixos.com/nix-darwin/option/homebrew.masApps
-        # masApps = [];
-        
-        onActivation.cleanup = "zap";
-        onActivation.autoUpdate = true;
-        onActivation.upgrade = true;
+        brews = [
+          # TODO: Add the Homebrew formulae (packages) you want to install
+          "neovim"                    # Better vim editor
+          "openjdk"                   # Java development tools
+          "git-delta"                 # Better git diff
+          "zoxide"                    # Better 'cd'
+          "openssl"                   # SSL libraries
+          "cmake"                     # build C++ code
+          "tldr"                      # Better man pages
+          "speedtest-cli"             # Internet speed test
+          "fd"                        # Improved 'find'
+          "git"                       # Git CLI tool
+          "yarn"                      # JavaScript package manager
+          "nvm"                       # Manage NodeJS versions
+          "zsh"                       # Z shell
+          "bat"                       # Better 'cat'
+          "imagemagick"               # Image manipulation tool
+          "python3"                   # Python
+          "sshpass"                   # SSH password manager
+          "fzf"                       # Fuzzy finder
+          "eza"                       # Better 'ls'
+          "spaceship"                 # Terminal theme (alternatively: 'starship')
+          "zsh-autocomplete"          # Autocompletion for terminal
+          "zsh-syntax-highlighting"   # Syntax highlighting for terminal
+          "zsh-autosuggestions"       # Autosuggestions for terminal
+        ];
+
+        casks = [
+          # TODO: Add the macOS applications (Casks) you want to install
+          "librewolf"                  # Better Firefox (alternative: brave-browser)
+          "viber"                      # Messaging app
+          "1password"                  # Password manager
+          "aldente"                    # (!PAID!) Battery saver (free alternative: battery)
+          "forklift"                   # (!PAID!) Finder replacement + SFTP client ( free SFTP client: cyberduck )
+          "redquits"                   # Quit apps with 'x'
+          "quit-all"                   # Quit a lot of apps quickly
+          "devutils"                   # (!PAID!) Developer utilities
+          "maccy"                      # Clipboard manager
+          "muzzle"                     # Silence notifications during screen share
+          "raycast"                    # Spotlight replacement with better search
+          "stats"                      # System stats in the menu bar
+          "clop"                       # File compression tool
+          "onyx"                       # Disk maintenance tool
+          "disk-drill"                 # Data recovery tool
+          "lulu"                       # Firewall
+          "protonvpn"                  # VPN client
+          "middleclick"                # emulate a scroll wheel click 
+          "readdle-spark"              # Email client
+          "bartender"                  # Hide apps in menu bar
+          "cleanshot"                  # (!PAID!) Screenshot tool (free alternative: shottr)
+          "alt-tab"                    # Command-tab alternative
+          "iina"                       # Video player
+          "keka"                       # Archive tool
+          "imageoptim"                 # Image optimization
+          "topnotch"                   # Hide menu bar notch
+          "betterdisplay"              # External monitor control
+          "keepingyouawake"            # Prevent the system from going into sleep
+          "nikitabobko/tap/aerospace"  # Window management tool (i3wm + "PopOS"-ness)
+          "postman"                    # API testing tool
+          "slack"                      # Messaging platform
+          "zoom"                       # Video conferencing tool
+          "discord"                    # Communication tool
+          "obsidian"                   # Note-taking app
+          "spotify"                    # Music streaming
+          "wezterm"                    # Terminal emulator
+          "visual-studio-code"         # Code editor
+          "clion"                      # C++ IDE
+          "pycharm-ce"                 # Python IDE
+          "intellij-idea-ce"           # Java IDE
+          "font-fira-code"            # Programming font  
+          "font-hack-nerd-font"       # Programming font & ICONS
+        ];
+
+        caskArgs.no_quarantine = true;
+
+        # Homebrew post-activation settings
+        onActivation.cleanup = "zap";     # Clean up after activation
+        onActivation.autoUpdate = true;   # Enable auto-update
+        onActivation.upgrade = true;      # Enable auto-upgrade
       };
 
+      services.nix-daemon.enable = true;
+      # Enable experimental Nix features
+      nix.settings.experimental-features = "nix-command flakes";
 
-      #DOCS: https://mynixos.com/options
-      security.pam.enableSudoTouchIdAuth = true;
-      system.startup.chime = false;
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+      system.stateVersion = 5;
 
-      # READ: https://macos-defaults.com/
-      # SYNTAX DOCS: https://mynixos.com/options/system.defaults
+      nixpkgs.hostPlatform = "aarch64-darwin";    # FIXME: Change to x86_64-darwin for Intel Macs
+
+      
+
+      # -------- macOS Default Settings --------
       system.defaults = {
-        dock.mineffect = "scale";
-        hitoolbox.AppleFnUsageType = "Show Emoji & Symbols";
-        screencapture.include-date = false;
-        SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
-        screensaver.askForPasswordDelay = 1;
-        screensaver.askForPassword = true;
-        screencapture.type = "jpg";
-        screencapture.location = "~/Screenshots";
-        screencapture.disable-shadow = true;
+
+        # READ: https://mynixos.com/options/system.defaults.dock
+        dock.mineffect = "scale";             # App minimize effect
+        dock.autohide = true;                 # Auto-hide the Dock
+        dock.autohide-delay = 0.0;            # Dock auto-hide delay (0.0 = instant)
+        dock.autohide-time-modifier = 0.0;    # Speed of Dock hide/show animation
+        dock.minimize-to-application = true;  # Minimize to app icon
+        dock.mru-spaces = false;              # Rearrange Spaces based on most recent use
+        dock.orientation = "left";            # Dock location on the screen
+        dock.show-recents = false;            # Show recent apps in dock
+        dock.showhidden = true;               # Show hidden app icons as translucent
+        dock.persistent-apps = [
+          # TODO: Add the paths of applications you want to keep in the Dock
+          # order: top to bottom / left to right
+          "/Applications/1Password.app"
+          "/Applications/LibreWolf.app"
+          "/Applications/Spark Desktop.app"
+          "/Applications/Discord.app"
+          "/Applications/Viber.app"
+          "/Applications/Slack.app"
+          "/Applications/Spotify.app"
+          "/Applications/Visual Studio Code.app"
+          "/Applications/PyCharm CE.app"
+          "/Applications/CLion.app"
+          "/Applications/IntelliJ IDEA CE.app"
+          "/Applications/Obsidian.app"
+          "/Applications/WezTerm.app"
+          "/System/Applications/System Settings.app"
+
+        ];
+
+        # READ: https://mynixos.com/options/system.defaults.NSGlobalDomain
         NSGlobalDomain.KeyRepeat = 2;
         NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
         NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = false;
@@ -68,55 +164,76 @@
         NSGlobalDomain.AppleScrollerPagingBehavior = true;
         NSGlobalDomain.AppleInterfaceStyle = "Dark";
         NSGlobalDomain.AppleICUForce24HourTime = true;
+
+        # READ: https://mynixos.com/options/system.defaults.menuExtraClock
         menuExtraClock.ShowSeconds = true;
         menuExtraClock.ShowDayOfWeek = true;
         menuExtraClock.ShowDate = 1;
         menuExtraClock.ShowAMPM = false;
         menuExtraClock.Show24Hour = true;
-        WindowManager.EnableStandardClickToShowDesktop = false;
-        loginwindow.GuestEnabled = false;
+
+        # READ: https://mynixos.com/options/system.defaults.finder
         finder.FXDefaultSearchScope = "SCcf";
         finder.FXPreferredViewStyle = "Nlsv";
         finder.ShowPathbar = true;
         finder.ShowStatusBar = true;
-        dock.autohide = true;
-        dock.autohide-delay = 0.0;
-        dock.autohide-time-modifier = 0.0;
-        dock.minimize-to-application = true;
-        dock.mru-spaces = false;
-        dock.orientation = "left";
-        dock.show-recents = false;
-        dock.showhidden = true;
-        dock.persistent-apps = dockApps;
+
+        # READ: https://mynixos.com/options/system.defaults.screencapture
+        screencapture.type = "jpg";
+        screencapture.location = "~/Screenshots";
+        screencapture.disable-shadow = true;
+        screencapture.include-date = false;
+
+        # READ: https://mynixos.com/options/system.defaults.screensaver
+        screensaver.askForPasswordDelay = 1;
+        screensaver.askForPassword = true;
+
+        # READ: https://mynixos.com/nix-darwin/option/system.defaults.WindowManager.EnableStandardClickToShowDesktop
+        WindowManager.EnableStandardClickToShowDesktop = false;
+
+        # READ: https://mynixos.com/nix-darwin/option/system.defaults.loginwindow.GuestEnabled
+        loginwindow.GuestEnabled = false;
+
+        # READ: https://daiderd.com/nix-darwin/manual/index.html#opt-system.defaults.hitoolbox.AppleFnUsageType
+        hitoolbox.AppleFnUsageType = "Show Emoji & Symbols";
+
+        # READ: https://daiderd.com/nix-darwin/manual/index.html#opt-system.defaults.SoftwareUpdate.AutomaticallyInstallMacOSUpdates
+        SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;    
       };
 
-      # --- DONT TOUCH ---
-      services.nix-daemon.enable = true;
-      nix.settings.experimental-features = "nix-command flakes";
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-      system.stateVersion = 5;
-      # ------------------
-
-      # IF NOT ARM/(APPLE SILICONE CHIP), CHANGE aarch64
-      nixpkgs.hostPlatform = "aarch64-darwin";
+      # -------- System Configuration --------
+      security.pam.enableSudoTouchIdAuth = true;  # Enable Touch ID for sudo authentication
+      system.startup.chime = false;               # Enable startup chime
     };
   in
   {
-    darwinConfigurations."${flake_device}" = nix-darwin.lib.darwinSystem {
+    # -------- Darwin Configuration --------
+    # FIXME: OTIONALLY CHANGE DEVICE NAME: "macbook-pro"
+    # make sure the the device name mathes up with:
+    # - - darwinPackages = self.darwinConfigurations."macbook-pro".pkgs - -
+    darwinConfigurations."macbook-pro" = nix-darwin.lib.darwinSystem {
       modules = [ 
         configuration
         nix-homebrew.darwinModules.nix-homebrew
         {
           nix-homebrew = {
+            # !-----------↓↓ DO NOT EDIT ↓↓----------!
             enable = true;
-            enableRosetta = true;
-            user = user;
-            autoMigrate = true;
+            # !-----------↑↑ DO NOT EDIT ↑↑----------!
+            enableRosetta = true;  # Enable Rosetta 2 for Apple Silicon
+            user = "babenkoilya";  # FIXME: Change this to your macOS username (use `whoami`)
+           
+            autoMigrate = true; 
+
           };
         }
       ];
     };
 
-    darwinPackages = self.darwinConfigurations."${flake_device}".pkgs;
+    # Darwin packages available for the configured system
+    # FIXME: OTIONALLY CHANGE DEVICE NAME: "macbook-pro"
+    # make sure the the device name mathes up with:
+    # - - darwinConfigurations."macbook-pro" = nix-darwin.lib.darwinSystem - -
+    darwinPackages = self.darwinConfigurations."macbook-pro".pkgs;
   };
 }
